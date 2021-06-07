7182c6895f85426660413730b680a74f05162ca9
solus <solus@internet.ru>
2021-06-07 10:34:06 +0300

    Add Spawn Rocket

--- Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STULauncherWeapon.cpp
@@ -2,4 +2,24 @@
 
 
 #include "Weapon/STULauncherWeapon.h"
+#include "Weapon/STUProjectile.h"
+#include "Kismet/GameplayStatics.h"
 
+void ASTULauncherWeapon::StartFire() 
+{
+	MakeShot();
+}
+
+void ASTULauncherWeapon::StopFire() 
+{
+
+}
+
+void ASTULauncherWeapon::MakeShot() 
+{
+	const FTransform SpawnTransform{FRotator::ZeroRotator, GetMuzzleWorldLocation()};
+	auto Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(GetWorld(), ProjectileClass, SpawnTransform);
+	// Set projectile params
+
+	UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
+}
