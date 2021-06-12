1ae9ad61f7ecc695acbd224cae1d502aad527d28
solus <solus@internet.ru>
2021-06-12 12:38:56 +0300

    Add AI Sight Sense

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -2,6 +2,13 @@
 
 #include "AI/STUAIController.h"
 #include "AI/STUAICharacter.h"
+#include "Components/STUAIPerceptionComponent.h"
+
+ASTUAIController::ASTUAIController()
+{
+	STUAIPerceptionComponent = CreateDefaultSubobject<USTUAIPerceptionComponent>("STUAIPerceptionComponent");
+	SetPerceptionComponent(*STUAIPerceptionComponent);
+}
 
 void ASTUAIController::OnPossess(APawn* InPawn)
 {
@@ -12,4 +19,11 @@ void ASTUAIController::OnPossess(APawn* InPawn)
 	{
 		RunBehaviorTree(STUCharacter->BehaviorTree);
 	}
+}
+
+void ASTUAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	const auto AimActor = STUAIPerceptionComponent->GetClosesEnemy();
+	SetFocus(AimActor);
 }
