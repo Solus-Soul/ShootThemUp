84aacf532e299d05312fad48364e69d8cf309ae8
solus <solus@internet.ru>
2021-06-14 16:56:51 +0300

    Add UI GameData

--- Source/ShootThemUp/Public/Player/STUPlayerState.h
+++ Source/ShootThemUp/Public/Player/STUPlayerState.h
@@ -19,10 +19,13 @@ public:
 	FLinearColor GetTeamColor() const { return TeamColor; }
 
 	void AddKill() { ++KillsNum; }
+	int32 GetKillsNum() const { return KillsNum; }
+
 	void AddDeaths() { ++DeathsNum; }
+	int32 GetDeathsNum() const { return DeathsNum; }
 
 	void LogInfo();
-	
+
 private:
 	int32 TeamID;
 	FLinearColor TeamColor;
